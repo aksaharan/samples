8fd53ae8fe02af00a177ea65bf0636662f96d0aa
Anil Kumar <anil@aks.rlogin.org>
2013-06-09 19:51:07 -0400

    Removed unused files

--- c++/feature-test/sizeof_empty_string_array.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(void) {
-	char temp[] = "";
-	std::cout << sizeof(temp) << std::endl;
-	return 0;
-}
---
+++ java/Test.java
@@ -0,0 +1,15 @@
+
+class Base {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+}
+
+public class Test extends Base {
+}
+
