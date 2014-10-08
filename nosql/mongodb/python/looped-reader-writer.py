5d59facbfd06d55510439f4e413a184f2f02e52f
Anil Kumar <aksaharan@yahoo.com>
2014-10-08 15:44:47 -0400

    changes for correct port and find operation

--- nosql/mongodb/python/looped-reader-writer.py
+++ nosql/mongodb/python/looped-reader-writer.py
@@ -3,13 +3,13 @@
 import pymongo
 import time
 
-client = pymongo.MongoClient("mongodb://localhost:20017,localhost:27018")
-
+#client = pymongo.MongoClient(["localhost:20017", "localhost:27018"])
+client = pymongo.MongoClient('localhost:27017,localhost:27018')
 db = client.test
 
 while 1 == 1:
 	try:
-		print "All Collections on DB: ", db.collection_names()
+		print "All Collections on DB: ", db.test.find_one()
 		time.sleep(10)
 	except Exception as e:
 		print "Exception caught: ", e
